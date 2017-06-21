c5ea9c9b89e3fd44e07a3b1dc33dd5d920fd1448
Keith Hubert <keith.hubert@gmail.com>
2017-06-21 18:27:39 -0400

    working on button

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -8,23 +8,33 @@ const Home = (props) => {
         <div className="wrapper">
           <h5>The World Is Your Playground</h5>
             <div className="center">
+
+          {props.current_user === null &&
             <a href="/users/sign_up">Sign Up</a>
+          }
+
           </div>
-              <video id="background-video" loop autoPlay>
-              <source src="https://s3.us-east-2.amazonaws.com/gathererbackground/animation.mp4" type="video/mp4" />
+            <video id="background-video" loop autoPlay>
+            <source src="https://s3.us-east-2.amazonaws.com/gathererbackground/animation.mp4" type="video/mp4" />
             Your browser does not support the video tag.
             </video>
-        </div>
+          </div>
       </div>
 
 
       <div className="summary">
         <p>Gathering is the first of it’s kind pairer for
-          Magic the Gathering players. Simply add
-          your location and find players near you.
+          Magic the Gathering players. Simply log in and find players near you.
+        </p>
+      </div>
+
+      <div className="footer">
+        <p> Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. /
+        Hasbro, Inc. This site is unaffiliated.
         </p>
       </div>
     </div>
+
   )
 }
 
