496ba52445104a8617b89196c36115bf030c368b
Keith Hubert <keith.hubert@gmail.com>
2017-07-25 03:07:20 -0400

    router

--- react/src/containers/App.js
+++ react/src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
 import Home from "../components/Home";
 import GameShowContainer from "./GameShowContainer";
-import HomeContainer from "./HomeContainer";
+import HomeContainer from "./homeContainer";
 import NearMeGameIndexContainer from "./NearMeGameIndexContainer";
 
 class App extends React.Component {
