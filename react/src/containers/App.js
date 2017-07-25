adce7bcf33431c02d3f15c082d81b803b8dea946
Keith Hubert <keith.hubert@gmail.com>
2017-07-24 22:16:11 -0400

    node engine

--- react/src/containers/App.js
+++ react/src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, browserHistory, IndexRoute} from 'react-router'
+import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
 import Home from "../components/Home";
 import GameShowContainer from "./GameShowContainer";
 import HomeContainer from "./HomeContainer";
