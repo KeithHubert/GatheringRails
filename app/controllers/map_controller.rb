80e673b60e5568e8e522dc4ef2a310463c2301e1
Keith Hubert <keith.hubert@gmail.com>
2017-07-26 01:40:56 -0400

    added d&t to marker, changed show marker date logic

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,14 +5,14 @@ class MapController < ApplicationController
 
   def index
     @user = current_user
-    @games = Game.where.not(lat: nil).where.not(lat: 0).where.not(lat: 1).where("date > ?", Time.zone.now + 1.day)
+    @games = Game.where.not(lat: nil).where.not(lat: 0).where.not(lat: 1).where("date > ?", Time.zone.now - 1.day)
     @unlisted = Game.where(lat: nil)
     @unlisted += Game.where(lat: 1)
     @hash = Gmaps4rails.build_markers(@games) do |game, marker|
       marker.lat game.lat
       marker.lng game.lng
       marker.title game.title
-      marker.infowindow "<h6><a href='/games/" + game.id.to_s + "'>" + game.title + "</a></h6><p>Address: " + game.address + "<br/>Description: " + game.gametype + "</p>"
+      marker.infowindow "<h6><a href='/games/" + game.id.to_s + "'>" + game.title + "</a></h6><p>Address: " + game.address + "<br/>Description: " + game.gametype + "</br>Date: " + game.date + "<br>Time: " + game.time + "</p>"
     end
   end
 end
